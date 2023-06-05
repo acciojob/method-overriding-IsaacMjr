e8cdae49bb12e1f7728168c47f8563b92010be87
isaacMjr <magamboisaac9@gmail.com>
2023-06-05 16:54:24 +0300

    another test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ public class Main {
         }
 
         public String meth() {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
 
